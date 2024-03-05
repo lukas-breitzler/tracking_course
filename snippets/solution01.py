8cbb7bfa23b736256790db3c126bf23bb0ed96ea
lukas-breitzler <159769407+lukas-breitzler@users.noreply.github.com>
2024-03-05 17:31:37 +0100

    Update solution01.py

--- snippets/solution01.py
+++ snippets/solution01.py
@@ -1,8 +1,19 @@
-#Or plot them separately 
-
+#uncomment above to load a solution
+ 
+# As always, let's first take a look where the animals went
+# for example by using an x,y, scatter plot
+# you can plot the two animals on top of each other in different colors for example.
+ 
+plt.scatter(an1[:,0],an1[:,1],color='g',s=0.01)
+plt.scatter(an2[:,0],an2[:,1],color='r',s=0.01)
+ 
+plt.figure()
+ 
+#or plot them separately
+ 
 fig,axes =plt.subplots(1,2, sharex=True, sharey=True) #create two subplots side by side
-
-axes[0].scatter(an1[:,0], an1[:,1], s=0.01, alpha=0.5, color='g') #plot the first animal in green
+ 
+axes[0].scatter(an1[:,0],an1[:,1],color='g',s=0.01, alpha = 1) #plot the first animal in green
 axes[0].set_aspect('equal') #set axes to equal scale
-axes[1].scatter(an2[:,0], an2[:,1], s=0.01, alpha=0.5, color='r') #plot the second animal in red
+axes[1].scatter(an2[:,0],an2[:,1],color='r',s=0.01, alpha = 1)
 axes[1].set_aspect('equal')
